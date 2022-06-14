4cc6794a86b71f47549fea388c4619b76df5316f
Tyler Henry <>
2022-06-14 13:04:12 -0400

    DIG-71364 Adding toggle class function

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 console.log('Engineering Training!');
 
+
+const modalContainer = document.getElementById('modalContainer');
 const modalButton = document.getElementById('modalButton');
 console.log("modalButton", modalButton);
+modalButton.addEventListener("click", ()=> {
+    console.log('Clicked!');
+    modalContainer.classList.toggle("hidden");
+});
