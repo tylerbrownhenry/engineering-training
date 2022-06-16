d85dad3460aeb60370e551f702cd40fe9b9326ac
Tyler Henry <>
2022-06-16 09:08:26 -0400

    DIG-71408 Adding close button inside of modal

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 console.log('Engineering Training!');
 
-
 const modalContainer = document.getElementById('modalContainer');
 const modalButton = document.getElementById('modalButton');
+
 console.log("modalButton", modalButton);
 modalButton.addEventListener("click", ()=> {
     console.log('Clicked!');
+    modalContainer.classList.toggle("hidden");
+});
+
+let closeModalButton = document.getElementsByClassName('closeModalButton');
+console.log("closeModalButton", closeModalButton);
+
+closeModalButton[0].addEventListener("click", ()=> {
     modalContainer.classList.toggle("hidden");
 });
