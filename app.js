6c9d086f3abf91f2bf67c36699c49d05cddecb4b
Tyler Henry <>
2022-07-13 09:24:22 -0400

    DIG-72390 Renderin html with express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const router = express.Router();
+const port = 3000
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
